1503fa2049db63ffc90144bb2082ea0e5cf2e1b8
avivost <ostrovsky.aviv@gmail.com>
2021-06-05 00:46:25 +0300

    created package

---
+++ package/pymagnification/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pymagnification',
+  packages = ['pymagnification'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'python wrapper to the windows magnification api',
+  author = 'Aviv Ostrovsky',
+  author_email = 'ostrovsky.aviv@gmail.com',
+  url = 'https://github.com/avivost',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['winuse32', 'pywin32', 'magnification','pymagnification'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
