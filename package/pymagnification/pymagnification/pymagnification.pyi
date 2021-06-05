a2cc9373e229f6b6164e7d9c88d6268410e0f147
avivost <ostrovsky.aviv@gmail.com>
2021-06-05 20:28:41 +0300

    added support for full  screen magnification + changed docstring format

--- package/pymagnification/pymagnification/pymagnification.pyi
+++ package/pymagnification/pymagnification/pymagnification.pyi
@@ -18,12 +18,19 @@ def MagSetWindowTransform(hwnd: int, matrix: Iterable[Iterable[float, float, flo
                                                       Iterable[float, float, float],
                                                       Iterable[float, float, float]]) -> int:
     """
-    Use to set the magnification transformation matrix on the window provided by the window handle
+    Sets the transformation matrix for a magnifier control.
 
-    :param hwnd: The handle of the magnification window.
-    :param matrix: A 3x3 matrix of the magnification transformation
-    :return
-    Returns 1 if successful, or 0 otherwise.
+    Parameters
+    ----------
+    hwnd
+        he handle of the magnification window.
+    matrix
+        A 3x3 magnification transformation matrix
+
+    Returns
+    -------
+    int
+        Returns 1 if successful, or 0 otherwise.
     """
 
 
@@ -31,56 +38,141 @@ def MagGetWindowTransform(hwnd: int) -> (Iterable[Iterable[float, float, float],
                                                   Iterable[float, float, float],
                                                   Iterable[float, float, float]]):
     """
-    Use to set the magnification transformation matrix on the window provided by the window handle
+    Retrieves the transformation matrix associated with a magnifier control.
 
-    :param hwnd: The handle of the magnification window.
-    :return
-    A 3x3 matrix of the magnification transformation
+    Parameters
+    ----------
+    hwnd
+        The handle of the magnification window.
+
+    Returns
+    -------
+    Iterable
+        A 3x3 matrix of the magnification transformation matrix
     """
 
 
 def MagSetWindowSource(hwnd: int, left: int, top: int, right: int, bottom: int) -> int:
     """
 
-    :param hwnd: The handle of the magnification window.
-    :param left: The left bound of the magnification rectangle
-    :param top: The top bound of the magnification rectangle
-    :param right: The right bound of the magnification rectangle
-    :param bottom: The bottom bound of the magnification rectangle
-    :return:
-    Returns 1 if successful, or 0 otherwise.
+    Parameters
+    ----------
+    hwnd
+        The handle of the magnification window.
+    left
+        The left bound of the magnification rectangle
+    top
+        The top bound of the magnification rectangle
+    right
+        The right bound of the magnification rectangle
+    bottom
+        The bottom bound of the magnification rectangle
+    Returns
+    -------
+    int
+        Returns 1 if successful, or 0 otherwise.
     """
 
 
 def MagGetWindowSource(hwnd: int) -> Tuple[int, int, int, int]:
     """
-    :param hwnd:The window handle.
-    :return: A tuple of the bounds of the magnified window rectangle(left, top, right , bottom )
+    Parameters
+    ----------
+    hwnd
+        The window handle.
+    Returns
+    -------
+    tuple
+        A tuple of the bounds of the magnified window rectangle(left, top, right , bottom)
 
     """
 
 
 def MagSetWindowFilterList(hwnd: int, dwFilterMode: int, count: int, pHWND: List[int]) -> int:
     """
-    
-    :param hwnd: The handle of the magnification window.
-    :param dwFilterMode: The magnification filter mode. It can be one of the following values:
-        MW_FILTERMODE_INCLUDE Note: This value is not supported on Windows 7 or newer.
-        MW_FILTERMODE_EXCLUDE
-    :param count: The number of window handles in the list.
-    :param pHWND: The list of window handles.
-    :return: 
-    Returns 1 if successful, or 0 otherwise.
+
+    Parameters
+    ----------
+    hwnd
+        The handle of the magnification window.
+    dwFilterMode
+        The magnification filter mode. It can be one of the following values:
+        # MW_FILTERMODE_INCLUDE (not supported on Windows 7 or newer.)
+        # MW_FILTERMODE_EXCLUDE
+    count
+        The number of window handles in the list.
+    pHWND
+        The list of window handles.
+    Returns
+    -------
+    int
+        Returns 1 if successful, or 0 otherwise
+
+
     """
 
 
 def MagGetWindowFilterList(hwnd: int) -> Tuple[int, int, List[int]]:
     """
 
-    :param hwnd: The magnification window.
-    :return:
-    A Tuple containing:
-    - count of window handles in the filter list, or -1 if the hwnd parameter is not valid.
-    - The filter mode.
-    - The list of window handles.
+    Parameters
+    ----------
+    hwnd
+        The magnification window.
+
+    Returns
+    -------
+    tuple
+        A Tuple containing:
+            - count of the window handles in the filter list, or -1 if the hwnd parameter is not valid.
+            - The filter mode.
+            - The list of window handles.
+
+
+    """
+
+
+def MagSetFullscreenTransform(magLevel: float, xOffset: int, yOffset: int) -> int:
+    """
+    Changes the magnification settings for the full-screen magnifier.
+
+    Parameters
+    ----------
+    magLevel
+        the full-screen magnifier factor, the value  needs to be between 1.0 and 4096.0
+        If this value is 1.0, the screen content is not magnified and no offsets are applied.
+
+    xOffset
+        The new x-coordinate offset, in pixels, for the upper-left corner of the magnified view.
+        The offset is relative to the upper-left corner of the primary monitor, in unmagnified coordinates.
+        The minimum value of the parameter is -262144, and the maximum value is 262144.
+
+    yOffset
+        The new y-coordinate offset, in pixels, for the upper-left corner of the magnified view.
+        The offset is relative to the upper-left corner of the primary monitor, in unmagnified coordinates.
+        The minimum value of the parameter is -262144, and the maximum value is 262144.
+
+    Returns
+    -------
+    int
+        Returns 1 if successful. Otherwise, 0.
+    """
+
+
+def MagGetFullscreenTransform() -> Tuple[float, int, int, int]:
+    """
+    Retrieves the magnification settings for the full-screen magnifier.
+
+    Returns
+    -------
+        tuple
+
+    magLevel : float
+        the full-screen magnifier factor
+    xOffset : int
+         the x-coordinate offset
+    yOffset : int
+         the y-coordinate offset
+    successStatus : int
+         1 if successful. Otherwise, 0.
     """
